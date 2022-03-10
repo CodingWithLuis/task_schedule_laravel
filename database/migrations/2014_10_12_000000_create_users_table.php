2fbb6f9da400a885946c6202a25feaf54724d8b4
Code with Luis <codewithluis11@gmail.com>
2022-03-09 20:17:04 -0600

    Add last_login_at and is_active fields to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->dateTime('last_login_at')->nullable();
+            $table->boolean('is_active')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
